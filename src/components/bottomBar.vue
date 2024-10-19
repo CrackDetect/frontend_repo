2692268bfec6b77ea292a3f038f240c8cbf60ee3
Seungho Kim <seunghok111@naver.com>
2024-10-19 19:15:29 +0900

    feat : bar components
    페이지 상단 검색바, 하단 페이지 이동바를 구성하는 component 작성

---
+++ src/components/bottomBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-bottom-navigation v-model="activeTab" app>
+      <v-btn
+        :to="{ name: 'homePage' }"
+        value="home"
+        icon
+      >
+        <v-icon>mdi-home</v-icon>
+        <span>home</span>
+      </v-btn>
+      <v-btn
+        :to="{ name: 'upLoadPage' }"
+        value="upload"
+        icon
+      >
+        <v-icon>mdi-upload</v-icon>
+        <span>upload</span>
+      </v-btn>
+      <v-btn
+        :to="{ name: 'listPage' }"
+        value="list"
+        icon
+      >
+        <v-icon>mdi-view-list</v-icon>
+        <span>list</span>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeTab: null,
+    };
+  },
+};
+</script>
---
+++ src/components/searchBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="page-container">
+    <div class="search-container">
+      <input type="text" v-model="searchText" class="custom-input" />
+      <button @click="search" class="custom-btn">검색</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchText: ''
+    };
+  },
+  methods: {
+    search() {
+      this.$router.push({name:'listPage', query:{ search: this.searchText} });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.page-container {
+  display: flex;
+  justify-content: flex-end; /* 페이지의 오른쪽으로 정렬 */
+  padding: 20px; /* 선택적으로 패딩을 추가하여 여백 조정 */
+}
+
+.search-container {
+  display: flex;
+  align-items: center; /* 수직 가운데 정렬 */
+}
+
+.custom-input {
+  margin-right: 10px; /* 입력 필드와 버튼 사이의 간격 */
+  border: 1px solid black; /* 테두리를 검정색으로 설정 */
+  padding: 5px;
+  border-radius: 4px;
+}
+
+.custom-btn {
+  padding: 5px 10px;
+  /*background-color: #3498db; 통일성 있는 버튼 색상 (파란색) */
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.custom-btn:hover {
+  background-color: grey;
+  /*background-color: #2980b9;호버 시 버튼 색상 변화 */
+}
+</style>
